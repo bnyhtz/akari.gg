85b118dcc0bab16e31d697f49442be5ede4c662f
Akari Le <contact@akari.plus>
2025-06-10 12:39:13 +0200

    Revert "nodejs_compat fix"
    This reverts commit 1b2f3e386fcef15a22c481fc6088e5a6eaf314f1.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    nodejs_compat: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
