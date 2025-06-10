1b2f3e386fcef15a22c481fc6088e5a6eaf314f1
Akari Le <contact@akari.plus>
2025-06-10 12:35:25 +0200

    nodejs_compat fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    nodejs_compat: true,
+  },
 };
 
 export default nextConfig;
